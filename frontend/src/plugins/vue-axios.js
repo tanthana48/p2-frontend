2f49f16901799af7b34ec0933566a9b0a40c6c8f
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-10-17 17:25:46 +0700

    connect to backend, fix dockerfile edit login/register page

--- frontend/src/plugins/vue-axios.js
+++ frontend/src/plugins/vue-axios.js
@@ -1,5 +1,35 @@
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import router from "@/router";
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
+
+// Add a request interceptor
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("userToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Token is expired
+      localStorage.removeItem("userToken");
+      router.push({ path: "/login" });
+    }
+    return Promise.reject(error);
+  },
+);
