ed17c66debeaa712f0a057cd81905475ef30c571
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-11-03 01:19:38 +0700

    add home page for viewing the video

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,114 @@
 <template>
-  <v-container> This is home. </v-container>
+  <v-container class="text-center">
+    <!-- Video Player -->
+    <div class="my-5">
+      <video ref="videoPlayer" class="video-js vjs-default-skin mx-auto" controls preload="auto" width="640" height="360"></video>
+    </div>
+
+    <!-- Video Thumbnails List -->
+    <div class="my-3" v-for="video in videos" :key="video.id" @click="loadVideo(video.hls_filename, video.id)">
+      <img :src="video.presignedThumbnailURL" width="480" height="270">
+      
+      <!-- Vuetify v-card for video information -->
+      <v-card class="mx-auto my-3" max-width="500">
+        <v-card-title>
+          <span class="headline">{{ video.title }}</span>
+        </v-card-title>
+        
+        <v-card-subtitle>
+          {{ video.description }}
+        </v-card-subtitle>
+
+        <v-card-actions>
+          <v-icon left small class="mr-2">mdi-eye</v-icon> {{ video.views }} views
+        </v-card-actions>
+      </v-card>
+
+    </div>
+  </v-container>
 </template>
 
-<script>
-export default {
-  name: "Home",
 
-  components: {},
+<script>
+import Vue from "vue";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+export default {
+  name: "VideoList",
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null,
+      player: null
+    };
+  },
+  mounted() {
+    this.fetchVideos();
+  },
+  beforeDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  },
+  methods: {
+    async fetchVideos() {
+      try {
+        const response = await Vue.axios.get("/api/videos");
+        const videosWithURLs = await Promise.all(response.data.videos.map(async video => {
+          video.presignedThumbnailURL = await this.generateThumbnailURL(video.thumbnail_filename);
+          return video;
+        }));
+        this.videos = videosWithURLs;
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }
+    },
+    async incrementViews(videoId) {
+        try {
+            await Vue.axios.post("/api/increment-views", { video_id: videoId });
+        } catch (error) {
+            console.error("Error incrementing views:", error);
+        }
+    },
+    async generateThumbnailURL(filename) {
+      try {
+        const response = await Vue.axios.post("/api/get-presigned-url-thumbnail", {thumbnail_filename: filename});
+        return response.data.presigned_url;
+      } catch (error) {
+        console.error("Error generating thumbnail URL:", error);
+      }
+    },
+    async loadVideo(hlsFilename, videoId) {
+      try {
+        const response = await Vue.axios.post("/api/get-presigned-m3u8", { hls_filename: hlsFilename });
+        const m3u8Content = response.data.m3u8_content;
+
+        const blob = new Blob([m3u8Content], { type: "application/vnd.apple.mpegurl" });
+        const url = URL.createObjectURL(blob);
+
+        if (!this.player) {
+          this.initializePlayer(url);
+        } else {
+          this.player.src({ type: "application/x-mpegURL", src: url });
+          this.player.load();
+          this.player.play();
+        }
+        await this.incrementViews(videoId);
+      } catch (error) {
+        console.error("Error fetching the m3u8 content:", error);
+      }
+    },
+    initializePlayer(url) {
+      this.player = videojs(this.$refs.videoPlayer, {
+        sources: [{
+          src: url,
+          type: "application/x-mpegURL"
+        }]
+      });
+
+      this.player.play();
+    }
+  }
 };
 </script>
