dd6135c4d3e4518e6f39f759ae4d4a4031e1b1fe
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-11-09 18:05:04 +0700

    add Myvideo page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -49,6 +49,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Register.vue"),
   },
+  {
+    path: "/video",
+    name: "MyVideo",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/MyVideo.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ frontend/src/views/MyVideo.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-container>
+      <h1>My Videos</h1>
+      <v-alert v-if="error" type="error" dismissible>{{ error }}</v-alert>
+      
+      <v-data-table
+        :headers="headers"
+        :items="videos"
+        class="elevation-1"
+        :loading="loading"
+        no-data-text="No videos found"
+      >
+        <template v-slot:[`item.actions`]="{ item }">
+          <v-btn color="red" icon @click="deleteVideo(item.id)">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </template>
+      </v-data-table>
+    </v-container>
+  </template>
+  
+  <script>
+  import Vue from "vue";
+  export default {
+    data() {
+      return {
+        videos: [],
+        loading: false,
+        error: null,
+        headers: [
+          { text: 'ID', value: 'id' },
+          { text: 'Title', value: 'title' },
+          { text: 'Description', value: 'description' },
+          { text: 'Date', value: 'date' },
+          { text: 'Views', value: 'views' },
+          { text: 'Status', value: 'status' },
+          { text: 'Actions', value: 'actions', sortable: false },
+        ],
+      };
+    },
+    created() {
+      this.fetchVideos();
+    },
+    methods: {
+      async fetchVideos() {
+        this.loading = true;
+        try {
+          const response = await Vue.axios.post('/api/myvideos', { username: $store.state.username });
+          this.videos = response.data.videos;
+        } catch (err) {
+          this.error = err.message;
+        } finally {
+          this.loading = false;
+        }
+      },
+      async deleteVideo(videoId) {
+        try {
+          await this.$http.delete(`/api/videos/${videoId}`);
+          this.fetchVideos();
+        } catch (err) {
+          this.error = err.message;
+        }
+      }
+    }
+  };
+  </script>
+  
