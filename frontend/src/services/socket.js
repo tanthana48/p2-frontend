5798cbde3c33aa46503d922e3bb1dbd85f47e314
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2023-11-17 01:12:47 +0700

    test socket

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const socketUrl = "ws://notification-service-service.default.svc:80";
+const socketUrl = "ws://localhost:8081";
 
 const socket = io(socketUrl, {
   path: "/socket.io",
